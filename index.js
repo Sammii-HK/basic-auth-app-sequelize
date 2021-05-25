319a8c70244f64824b5fb30b30d32b16de4cec22
Samantha Kellow <sammii.h@icloud.com>
2021-05-25 22:24:53 +0100

    apply Hapi basic strategy to `index.js` server

--- index.js
+++ index.js
@@ -2,15 +2,43 @@
 
 const Hapi = require('@hapi/hapi');
 const Routes = require('./controller');
+const Bcrypt = require('bcrypt');
+const db = require('./models');
 
-const init = async () => {
+
+const validate = async (request, username, password) => {
+  try {
+      // const user = db.users[username];
+      const user = await db.User.findAll({
+        where: { username },
+      });
+      if (!user) {
+          return { credentials: null, isValid: false };
+      }
+    
+      const isValid = await Bcrypt.compare(password, user.password);
+      const credentials = { id: user.id, name: user.name };
+    
+      return { isValid, credentials };
+  } catch (e) {
+    console.log('error validating user:', e);
+    return h.response(`Failed: ${e.message}`).code(500);
+  }
+};
+
+const start = async () => {
   const server = Hapi.Server({
     host: `localhost`,
     port: 3000,
   });
+
+  await server.register(require('@hapi/basic'));
+  server.auth.strategy('simple', 'basic', { validate });
+
   server.route(Routes);
+
   await server.start();
   console.log('🦄 Server running at port:', server.info.uri);
 };
 
-init();
+start();
