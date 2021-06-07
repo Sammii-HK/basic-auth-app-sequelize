38a9b1771da16b7571aaf6a0166092b23abe0c29
Samantha Kellow <sammii.h@icloud.com>
2021-06-07 15:27:29 +0100

    removing all package auth from server to clean up and reimplement

--- index.js
+++ index.js
@@ -4,39 +4,14 @@ const Hapi = require('@hapi/hapi');
 const Routes = require('./controller');
 const Bcrypt = require('bcrypt');
 const db = require('./models');
+const { port } = require('./config/environment')
 
-
-const validate = async (request, username, email, password) => {
-  try {
-      // const user = db.users[username];
-      const user = await db.User.findAll({
-        where: { username, email },
-      });
-      console.log("user", user);
-      
-      if (!user) {
-          return { credentials: null, isValid: false };
-      }
-    
-      const isValid = await Bcrypt.compare(password, user.password);
-      const credentials = { id: user.id, name: user.name };
-    
-      return { isValid, credentials };
-  } catch (e) {
-    console.log('error validating user:', e);
-    return h.response(`Failed: ${e.message}`).code(500);
-  }
-};
 
 const start = async () => {
   const server = Hapi.Server({
     host: `localhost`,
-    port: 3000,
+    port: port,
   });
-
-  await server.register(require('@hapi/basic'));
-  server.auth.strategy('simple', 'basic', { validate });
-  server.auth.default('simple')
 
   server.route(Routes);
 
